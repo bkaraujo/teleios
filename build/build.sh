a03b381a216e092d9f82b431a4ddec699d78866b
Bruno da Silva Perez Araújo <bkraujo@gmail.com.br>
2025-03-19 08:37:31 -0300

    ...

--- build/build.sh
+++ build/build.sh
@@ -3,17 +3,27 @@
 clear
 ROOTFS="$(dirname "$(realpath -s "$0")")/.."
 BUILDFS="$ROOTFS/cmake-build-debug"
+# -----------------------------------------
+# (Re)create the build directory
+# -----------------------------------------
 if [[ ! -z $1 && $1 -eq "clean" ]]; then
   rm -rf $BUILDFS
 fi
 
 if [[ ! -e $BUILDFS ]]; then
+  echo "[TELEIOS] Clean ... "
    mkdir -p $BUILDFS ; cd $BUILDFS
-   cmake -G Ninja $ROOTFS
+   echo "cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja -S $ROOTFS -B $BUILDFS"
+   cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja -S $ROOTFS -B $BUILDFS
 fi
-
+# -----------------------------------------
+# Build the target
+# -----------------------------------------
+echo "[TELEIOS] Build ... "
 cd $BUILDFS
-cmake --build . -- -j$(expr $(hwinfo --cpu --short | wc -l) - 1)
+
+echo "cmake --build $BUILDFS -j$(expr $(hwinfo --cpu --short | wc -l) - 1)"
+cmake --build $BUILDFS -j$(expr $(hwinfo --cpu --short | wc -l) - 1)
 RESULT=$?
 
 if [[ $RESULT -ne 0 ]]; then
--- build/run.sh
+++ build/run.sh
@@ -3,5 +3,12 @@
 clear
 ROOTFS="$(dirname "$(realpath -s "$0")")/.."
 BUILDFS="$ROOTFS/cmake-build-debug"
-
+# -----------------------------------------
+# Build the target
+# -----------------------------------------
+${ROOTFS}/build/build.sh
+# -----------------------------------------
+# Execute the target
+# -----------------------------------------
+echo "[TELEIOS] Run ... "
 $BUILDFS/teleios $1
