f01dfb0b4c900ac8b164206db209548c3e353913
Bruno da Silva Perez Araújo <bruno.araujo@funpresp.com.br>
2025-11-17 11:00:57 -0300

    Log Level defined by application.yml

--- engine/src/main/teleios/config.c
+++ engine/src/main/teleios/config.c
@@ -10,6 +10,8 @@ b8 tl_config_initialize() {
     m_allocator = tl_memory_allocator_create(TL_KIBI_BYTES(4), TL_ALLOCATOR_LINEAR);
     m_properties = tl_map_create(m_allocator, 32);
     tl_serializer_walk();
+
+    tl_logger_set_level(tl_config_get_log_level("teleios.logging.level"));
     TL_PROFILER_POP_WITH(true)
 }
 
